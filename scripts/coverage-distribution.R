3d7556986003c8c22e0490d5b6fc12a551d3cad1
Czirion <claudia.zirionmartinez@duke.edu>
2023-11-10 09:48:12 -0500

    rename R scripts

---
+++ scripts/coverage-distribution.R
@@ -0,0 +1,28 @@
+log <- file(snakemake@log[[1]], open="wt")
+sink(log, type = "output")
+sink(log, type = "message")
+
+suppressPackageStartupMessages(library(tidyverse))
+library(RColorBrewer)
+suppressPackageStartupMessages(library(scales))
+library(svglite)
+
+print("Reading TSV file")
+cov<- read.delim("Downloads/cov.tsv", header = FALSE, col.names = c("Chromosome", "Range", "Coverage", "Count"), stringsAsFactors = TRUE)
+
+print("Plotting Coverage distribution")
+
+chrom_colors <- c(brewer.pal(nlevels(cov$Chromosome)/2, "Dark2"),brewer.pal(nlevels(cov$Chromosome)/2, "Set2"))
+
+plot <- ggplot(cov, aes(x=Coverage, y=Count))+
+  geom_line(aes(color = Chromosome))+
+  scale_color_manual(values = chrom_colors)+
+  facet_wrap(~Chromosome,ncol = 2)+
+  scale_y_continuous(name = "Number of Sites", labels = comma)+
+  theme_light()+
+  theme(legend.position="none")
+
+print("Saving plot")
+ggsave("Downloads/cov_distribution.svg", plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+ggsave(snakemake@output[[1]], plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+print("Done!")
