bca8d5fe340d5484bc9ab3b043386b4f7891733d
Czirion <claudia.zirionmartinez@duke.edu>
2023-11-29 09:24:36 -0500

    change location of samples.txt

--- scripts/getcds.sh
+++ scripts/getcds.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-parallel "seqkit faidx genomes-annotations/{}/predicted_cds.fa $1 | seqkit replace -p '($)' -r ' sample={}'"  :::: results/samples.txt > cds/$1.fa
+parallel "seqkit faidx genomes-annotations/{}/predicted_cds.fa $1 | seqkit replace -p '($)' -r ' sample={}'"  :::: samples.txt > cds/$1.fa
--- scripts/getprots.sh
+++ scripts/getprots.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-parallel "seqkit faidx genomes-annotations/{}/predicted_proteins.fa $1 | seqkit replace -p '($)' -r ' sample={}'"  :::: results/samples.txt > proteins/$1.fa
+parallel "seqkit faidx genomes-annotations/{}/predicted_proteins.fa $1 | seqkit replace -p '($)' -r ' sample={}'"  :::: samples.txt > proteins/$1.fa
