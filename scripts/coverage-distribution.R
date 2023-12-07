199d17439781b884f496e7533e299bafdd3c9498
Czirion <claudia.zirionmartinez@duke.edu>
2023-12-07 14:09:47 -0500

    Change color and add title

--- scripts/coverage-distribution.R
+++ scripts/coverage-distribution.R
@@ -7,34 +7,36 @@ library(RColorBrewer)
 suppressPackageStartupMessages(library(scales))
 library(svglite)
 
-print("Reading TSV file")
-cov<- read.csv(snakemake@input[[1]], header = TRUE, stringsAsFactors = TRUE)
-#setwd("./genomes-annotations/SRS881221/")
+print("Reading files and joining data with chromosome names")
 #setwd("./genomes-annotations/SRS404449/")
+# cov<- read.csv("cov.csv", header = TRUE, stringsAsFactors = TRUE)
+# chrom_names <- read.csv("../../chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
+# sample <- "/analysis/czirion/Crypto_Diversity_Pipeline/genomes-annotations/SRS404449/cov.csv"
+sample <- snakemake@input[[1]]
+Split <- str_split(sample, "/")
+sample <- Split[[1]][length(Split[[1]])-1]
 
-#cov<- read.csv("cov.csv", header = TRUE, stringsAsFactors = TRUE)
-
+cov<- read.csv(snakemake@input[[1]], header = TRUE, stringsAsFactors = TRUE)
+chrom_names <- read.csv(snakemake@input[[2]], header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
 cov <-cov %>%
   rename(Accession = Chromosome)
-
-#chrom_names <- read.csv("../../chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
-chrom_names <- read.csv("chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
 cov <- left_join(cov, chrom_names, by = "Accession")
 
 print("Plotting Coverage distribution")
-
-#chrom_colors <- c(brewer.pal(nlevels(cov$Chromosome)/2, "Dark2"),brewer.pal(nlevels(cov$Chromosome)/2, "Set2"))
+data_color = "aquamarine4"
+lineage <- levels(as.factor(cov$Lineage))
 
 plot <- ggplot(cov, aes(x=Coverage, y=Count))+
-  geom_col(aes(color = Chromosome))+ 
-  #scale_color_manual(values = chrom_colors)+
+  geom_col(fill=data_color)+ 
   facet_wrap(~Chromosome,ncol = 2)+
   scale_y_log10(name = "Number of Sites", labels = comma)+
   scale_x_continuous(name = "Coverage (X) ", labels = comma, n.breaks = 10)+
   theme_light()+
-  theme(legend.position="none")
-#ggsave("cov_distribution.svg", plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+  theme(legend.position="none")+
+  labs(title = paste(lineage, sample,  sep = " "))
+
+#ggsave("../../cov_distribution.svg", plot = plot, dpi = 50, units = "cm", height = 22, width = 22)
 
 print("Saving plot")
-ggsave(snakemake@output[[1]], plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+ggsave(snakemake@output[[1]], plot = plot, dpi = 50, units = "cm", height = 22, width = 22)
 print("Done!")
--- scripts/mapq-distribution.R
+++ scripts/mapq-distribution.R
@@ -7,39 +7,38 @@ library(RColorBrewer)
 suppressPackageStartupMessages(library(scales))
 library(svglite)
 
-print("Reading TSV file")
-mapq<- read.csv(snakemake@input[[1]], header = TRUE, stringsAsFactors = TRUE)
-#setwd("./genomes-annotations/SRS881221/")
+print("Reading files and joining data with chromosome names")
 #setwd("./genomes-annotations/SRS404449/")
+# mapq<- read.csv("mapq.csv", header = TRUE, stringsAsFactors = TRUE)
+# chrom_names <- read.csv("../../chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
+# sample <- "/analysis/czirion/Crypto_Diversity_Pipeline/genomes-annotations/SRS404449/mapq.csv"
+sample <- snakemake@input[[1]]
+Split <- str_split(sample, "/")
+sample <- Split[[1]][length(Split[[1]])-1]
 
-#mapq<- read.csv("mapq.csv", header = TRUE, stringsAsFactors = TRUE)
-
+mapq<- read.csv(snakemake@input[[1]], header = TRUE, stringsAsFactors = TRUE)
+chrom_names <- read.csv(snakemake@input[[2]], header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
 mapq <-mapq %>%
   rename(Accession = Chromosome)
-#chrom_names <- read.csv("../../chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
-
-chrom_names <- read.csv("chromosome_names.csv", header = FALSE, col.names = c("Lineage", "Accession", "Chromosome"))
-
 mapq <- left_join(mapq, chrom_names, by = "Accession")
 
-
 print("Plotting MAPQ distribution")
-
-#chrom_colors <- c(brewer.pal(nlevels(mapq$Chromosome)/2, "Dark2"),brewer.pal(nlevels(mapq$Chromosome)/2, "Set2"))
+data_color = "aquamarine4"
+lineage <- levels(as.factor(mapq$Lineage))
 
 plot <- ggplot(mapq, aes(x=MAPQ, y=Count))+
-  geom_col(aes(fill = Chromosome))+
-  #scale_fill_manual(values = chrom_colors)+
+  geom_col(fill=data_color)+
   facet_wrap(~Chromosome,ncol = 2)+
   scale_y_log10(name = "Number of Reads", labels = comma, breaks = 100 * 10^seq(0,4, by = 2))+
   scale_x_continuous(name = "Mapping Quality", n.breaks = 8)+
   theme_light()+
-  theme(legend.position="none")
+  theme(legend.position="none")+
+  labs(title = paste(lineage, sample,  sep = " "))
 
-#ggsave("mapq_distribution.svg", plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+#ggsave("../../mapq_distribution.svg", plot = plot, dpi = 50, units = "cm", height = 22, width = 22)
 
 print("Saving plot")
-ggsave(snakemake@output[[1]], plot = plot, dpi = 200, units = "cm", height = 22, width = 22)
+ggsave(snakemake@output[[1]], plot = plot, dpi = 50, units = "cm", height = 22, width = 22)
 print("Done!")
 
 # Binned version
