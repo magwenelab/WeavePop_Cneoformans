14e037e17dfd514c53b02d3f34679072894e1eaf
Czirion <claudia.zirionmartinez@duke.edu>
2023-12-19 20:47:15 -0500

    use files/ dir name

--- get-chromosome-names.sh
+++ get-chromosome-names.sh
@@ -1,6 +1,6 @@
 #/usr/bin/env bash
   
-cut -d',' -f4 lineage_references.csv | tail -n4 | while read file
+cut -d',' -f4 files/lineage_references.csv | tail -n4 | while read file
     do 
         lineage=$(echo $file | cut -d'.' -f1)
         grep ">" references/$file | cut -d',' -f1 | cut -d'>' -f2 |  while read line
@@ -9,9 +9,11 @@ cut -d',' -f4 lineage_references.csv | tail -n4 | while read file
             name=$(echo $line | rev | cut -d' ' -f1 | rev)
             echo $lineage,$accession,$name
         done
-    done > chromosome_names.csv
+    done > tmp
 
-sed -i 's/contig/2\./g' chromosome_names.csv # To edit the names of the chromosome 2 contigs of Crypto VNBI
-sed -i 's/(3;11)/3/g' chromosome_names.csv 
-sed -i 's/(11;3)/11/g' chromosome_names.csv 
-sed -i 's/,0/,/g' chromosome_names.csv 
+sed -i 's/contig/2\./g' tmp # To edit the names of the chromosome 2 contigs of Crypto VNBI
+sed -i 's/(3;11)/3/g' tmp
+sed -i 's/(11;3)/11/g' tmp 
+sed -i 's/,0/,/g' tmp 
+awk -F ',' '{ gsub("0000", "0", $3) ; print }' tmp > files/chromosome_names.csv
+rm tmp
