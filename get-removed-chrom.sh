4dcc1ddd62c4604105fec9f2941bd737c16b6263
Czirion <claudia.zirionmartinez@duke.edu>
2024-01-11 14:52:54 -0500

    remove from gff

--- get-removed-chrom.sh
+++ get-removed-chrom.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 #
-# Remove chromosome/contig from fasta file
-# Keeping the original as name.fasta.original
+# Remove chromosome/contig from reference genome (fasta and gff)
+# Keeping the original as name.fasta.original and name.gff.original
 
-scriptargs="path-to-fasta seq_id"
-nexpargs=2
+scriptargs="path-to-fasta path-to-gff seq_id"
+nexpargs=3
 nargs=$#
 
 if [[ $nargs -ne $nexpargs ]]
@@ -16,9 +16,15 @@ then
 fi
 
 fasta=$1
-seqid=$2
+gff=$2
+seqid=$3
 
 echo "Removing $seqid from $fasta"
 seqkit grep -n -r -v -p ${seqid} ${fasta} > ${fasta}.modif
 mv ${fasta} ${fasta}.original
 mv ${fasta}.modif ${fasta}
+
+echo "Removing $seqid from $gff"
+grep -v $seqid $gff >${gff}.modif
+mv ${gff} ${gff}.original
+mv ${gff}.modif ${gff}
