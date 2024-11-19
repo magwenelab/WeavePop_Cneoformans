f563517fa2c7c995c790a4d647dee23648a2121c
Czirion <claudia.zirionmartinez@duke.edu>
2024-11-19 17:35:58 -0500

    add tree

--- analyses/cnv_plot/desjardins/scripts/aneuploidies.R
+++ analyses/cnv_plot/desjardins/scripts/aneuploidies.R
@@ -2,6 +2,10 @@ setwd("/FastData/czirion/Crypto_Diversity_Pipeline/analyses/cnv_plot/desjardins/
 
 library(tidyverse)
 library(ggbeeswarm)
+library(ggtree)
+library(ggtreeExtra)
+library(ape)
+
 cnv <- read.delim("/FastData/czirion/Crypto_Diversity_Pipeline/Crypto_Ashton/results_joined_2024-10-16/02.Dataset/cnv/cnv_calls.tsv", header=TRUE , sep="\t")
 chromosome_lengths = read.delim("../data/chromosome_lengths.tsv", header=FALSE, col.names=c("Accession", "length"), sep="\t")
 chromosome_names = read.delim("/FastData/czirion/Crypto_Diversity_Pipeline/Crypto_Desjardins/config/chromosomes.csv", header=TRUE, sep=",")
@@ -103,6 +107,10 @@ metadata <- metadata %>%
     ungroup()
 
 chromosome_names = read.delim("/FastData/czirion/Crypto_Diversity_Pipeline/Crypto_Desjardins/config/chromosomes.csv", header=TRUE, sep=",")
+chromosome_names <- chromosome_names %>%
+    mutate(chromosome = as.factor(chromosome))
+    
+levels(chromosome_names$chromosome) <- paste("chr", chromosome_names$chromosome, sep="")
 
 duplicated <- depth_by_chrom_good %>%
     filter(norm_chrom_median > 1.55)%>%
@@ -117,16 +125,43 @@ duplicated_summary <- duplicated %>%
     mutate(percent_samples = round((n_samples / samples_per_lineage) * 100, 1))%>%
     select(lineage, chromosome, n_samples, percent_samples)
 
+df_info <- duplicated %>%
+    select(strain, chromosome)%>%
+    mutate(duplicated = 1)%>%
+    arrange(chromosome)%>%
+    pivot_wider(names_from = chromosome, values_from = duplicated, values_fill = 0)%>%
+    column_to_rownames("strain")
 
-deleted <- depth_by_chrom_good %>%
-    filter(norm_chrom_median < 0.55)%>%
-    left_join(metadata, by="sample")%>%
-    left_join(chromosome_names, by=c("accession", "lineage"))%>%
-    select(lineage,samples_per_lineage,sample,strain, source, accession, chromosome, norm_chrom_median)
+df_info <- df_info %>%
+    mutate(across(everything(), ~ ifelse(. == 1, cur_column(), NA)))
 
-deleted_summary <- deleted %>%
-    group_by(lineage, chromosome) %>%
-    summarise(n_samples = n_distinct(sample), samples_per_lineage = first(samples_per_lineage))%>%
-    arrange(desc(n_samples))%>%
-    mutate(percent_samples = round((n_samples / samples_per_lineage) * 100, 1))%>%
-    select(lineage, chromosome, n_samples, percent_samples)
+
+
+
+tree_path <- "/FastData/czirion/Crypto_Diversity_Pipeline/analyses/cnv_plot/desjardins/data/CryptoDiversity_Desjardins_Tree.tre"
+tree <- read.tree(tree_path)
+
+outgroup_clade <- metadata %>%
+    filter(lineage == "VNII")%>%
+    select(strain)
+
+outgroup_clade <- outgroup_clade$strain
+tree <- root(tree, outgroup = outgroup_clade)
+
+
+p <- ggplot(tree, aes(x, y)) + 
+    geom_tree() + 
+    geom_tiplab(aes(label = label), size = 2) +
+    theme_tree()
+
+
+p <- ggtree(tree, layout = "circular") + 
+    # geom_tiplab(aes(label = label), size = 2) +
+    theme_tree()
+
+gheatmap(p, df_info, offset=.8, width=.2,
+               colnames_angle=95, colnames_offset_y = .25) +
+    scale_fill_viridis_d(option="D", name="Duplicated\nchromosome")
+
+gheatmap(p, df_info,width=.2,) +
+    scale_fill_viridis_d(option="D", name="Duplicated\nchromosome")
