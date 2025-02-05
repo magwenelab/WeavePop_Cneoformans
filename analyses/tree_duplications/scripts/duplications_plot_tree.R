a35303e551128ae788c3378bd30b08b77ff2b39d
Czirion <claudia.zirionmartinez@duke.edu>
2025-02-04 20:04:52 -0500

    change colors and make tree with only duplicated strains

--- analyses/tree_duplications/scripts/duplications_plot_tree.R
+++ analyses/tree_duplications/scripts/duplications_plot_tree.R
@@ -78,30 +78,44 @@ tree <- drop.tip(tree, setdiff(tree$tip.label, metadata$strain))
 # display.brewer.all(colorblindFriendly = TRUE)
 # display.brewer.all(colorblindFriendly = FALSE)
 
+dataset_colors <- c("white", brewer.pal(9, "Set1")[c(1, 2)])
+names(dataset_colors) <- levels(as.factor(metadata$dataset))
+
+lineage_colors <- brewer.pal(8, "Dark2")[c(1, 2, 3, 4)]
+names(lineage_colors) <- levels(as.factor(metadata$lineage))
+
+sublineage_colors <- c("white", brewer.pal(12, "Set3")[c(1:10)])
+names(sublineage_colors) <- levels(as.factor(metadata$vni_subdivision))
+
 chrom_colors <- c(brewer.pal(nlevels(duplications$chromosome), "Paired"), "grey93")
 names(chrom_colors) <- c(levels(duplications$chromosome), "Euploid")
+
+source_colors <- brewer.pal(11, "BrBG")[c(9, 3)] # 9, 3 are the colors for the two sources
+names(source_colors) <- levels(as.factor(metadata$source))
+
+
 
 p <- ggtree(tree, layout = "circular", size = 0.1, branch.length = "none") + 
     geom_tiplab(aes(label = label), size = 0.2, align =TRUE, 
                     linetype = "dashed", linesize = .05)
 
 p1 <- gheatmap(p, dataset, width=.05, colnames=FALSE, offset=3) +
-    scale_fill_brewer(palette = "Set1", name="Dataset",  na.translate = FALSE)+ 
+    scale_fill_manual(values = dataset_colors, name="Dataset", na.translate = FALSE)+
     guides(fill = guide_legend(order = 1))+
     new_scale_fill()
 
 p2 <- gheatmap(p1, lineage, width=.05, colnames=FALSE, offset=5) +
-    scale_fill_brewer(palette = "Dark2", name="Lineage",  na.translate = FALSE)+ 
+    scale_fill_manual(values = lineage_colors, name="Lineage", na.translate = FALSE)+
     guides(fill = guide_legend(order = 2))+
     new_scale_fill()
 
 p3 <- gheatmap(p2, sublineage, width=.05, colnames=FALSE, offset=7,) +
-    scale_fill_brewer(palette="Set3", name="VNI Sublineage", na.translate = FALSE)+ 
+    scale_fill_manual(values = sublineage_colors, name="VNI Sublineage", na.translate = FALSE)+
     guides(fill = guide_legend(order = 3))+
     new_scale_fill()
 
 p4 <- gheatmap(p3, source, width=.05, colnames=FALSE, offset=9,) +
-        scale_fill_brewer(palette="Set2", name="Source", na.translate = FALSE)+ 
+        scale_fill_manual(values = source_colors, name="Source", na.translate = FALSE)+
         guides(fill = guide_legend(order = 4))+
         new_scale_fill()
 
@@ -114,39 +128,61 @@ p5 <- gheatmap(p4, dup_chroms, width=.32, colnames = FALSE, offset=11,) +
         legend.text=element_text(size=7),
         legend.key.size = unit(0.3, "cm"),
         plot.margin = margin(0, 0, 0, 0, "cm"))
-
+p5
 ggsave("/FastData/czirion/Crypto_Diversity_Pipeline/analyses/tree_duplications/results/figures/tree_merged_duplications.png", p5, height = 7, width = 7, units = "in", dpi = 900)
 
-# Chromosomes 12, 13
+#### Plot tree with duplications of chromosomes 12 and 13 ####
 dup_chroms_12_13 <- dup_chroms %>%
     select(chr12, chr13)
 
-p <- ggtree(tree, layout = "circular", size = 0.1, branch.length = "none") + 
-    geom_tiplab(aes(label = label), size = 0.2, align =TRUE, 
-                    linetype = "dashed", linesize = .05)
-
-p1 <- gheatmap(p, dataset, width=.05, colnames=FALSE, offset=3) +
-    scale_fill_brewer(palette = "Set1", name="Dataset",  na.translate = FALSE)+ 
-    new_scale_fill()
-
-p2 <- gheatmap(p1, lineage, width=.05, colnames=FALSE, offset=5) +
-    scale_fill_brewer(palette = "Dark2", name="Lineage",  na.translate = FALSE)+ 
-    new_scale_fill()
-
-p3 <- gheatmap(p2, sublineage, width=.05, colnames=FALSE, offset=7,) +
-    scale_fill_brewer(palette="Set3", name="VNI Sublineage", na.translate = FALSE)+ 
-    new_scale_fill() 
-
-p4 <- gheatmap(p3, source, width=.05, colnames=FALSE, offset=9,) +
-        scale_fill_brewer(palette="Set2", name="Source", na.translate = FALSE)+ 
-        new_scale_fill()
-
 p5 <- gheatmap(p4, dup_chroms_12_13, width=.1, colnames = FALSE, offset=11,) +
-    scale_fill_manual(values = chrom_colors, name="Duplicated\nchromosomes", na.value = "white")+
-    theme(legend.title = element_text( size=7),
-        legend.text=element_text(size=5),
+    scale_fill_manual(values = chrom_colors, name="Duplicated\nchromosomes", na.translate = FALSE )+
+    guides(fill = guide_legend(order = 5))+
+    theme(legend.position = "right",
+        legend.direction = "vertical",
+        legend.title = element_text(size=9),
+        legend.text=element_text(size=7),
         legend.key.size = unit(0.3, "cm"),
         plot.margin = margin(0, 0, 0, 0, "cm"))
-
+p5
 ggsave("/FastData/czirion/Crypto_Diversity_Pipeline/analyses/tree_duplications/results/figures/tree_merged_duplications_12_13.png", p5, height = 7, width = 7, units = "in", dpi = 900)
 
+#### Plot the tree with only the samples that have duplications and the references####
+keep_strains <- c(levels(duplications_full$strain), "H99", "Bt22")
+tree_dups <- drop.tip(tree, setdiff(tree$tip.label, keep_strains))
+
+p <- ggtree(tree_dups, layout = "circular", size = 0.5, branch.length = "none") + 
+    geom_tiplab(aes(label = label), size = 1, align =TRUE, 
+                    linetype = "dashed", linesize = 0.1, offset = 2)
+
+p1 <- gheatmap(p, dataset, width=0.1, colnames=FALSE, offset=9) +
+    scale_fill_manual(values = dataset_colors, name="Dataset", na.translate = FALSE)+
+    guides(fill = guide_legend(order = 1))+
+    new_scale_fill()
+
+p2 <- gheatmap(p1, lineage, width=0.1, colnames=FALSE, offset=10.5) +
+    scale_fill_manual(values = lineage_colors, name="Lineage", na.translate = FALSE)+
+    guides(fill = guide_legend(order = 2))+
+    new_scale_fill()
+
+p3 <- gheatmap(p2, sublineage, width=.1, colnames=FALSE, offset=12) +
+    scale_fill_manual(values = sublineage_colors, name="VNI Sublineage", na.translate = FALSE)+ 
+    guides(fill = guide_legend(order = 3))+
+    new_scale_fill()
+
+p4 <- gheatmap(p3, source, width=.1, colnames=FALSE, offset=13.5,) +
+        scale_fill_manual(values = source_colors, name="Source", na.translate = FALSE)+
+        guides(fill = guide_legend(order = 4))+
+        new_scale_fill()
+
+p5 <- gheatmap(p4, dup_chroms, width=.5, colnames = FALSE, offset=15.3,) +
+    scale_fill_manual(values = chrom_colors, name="Duplicated\nchromosomes", na.translate = FALSE )+
+    guides(fill = guide_legend(order = 5))+
+    theme(legend.position = "right",
+        legend.direction = "vertical",
+        legend.title = element_text(size=9),
+        legend.text=element_text(size=7),
+        legend.key.size = unit(0.5, "cm"),
+        plot.margin = margin(0, 0, 0, 0, "cm"))
+
+ggsave("/FastData/czirion/Crypto_Diversity_Pipeline/analyses/tree_duplications/results/figures/tree_merged_duplications_only_duplicated.png", p5, height = 7, width = 7, units = "in", dpi = 900)
