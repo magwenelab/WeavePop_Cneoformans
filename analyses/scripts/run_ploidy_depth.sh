7953f72e10e12490c636efe248396374b2a42c10
Czirion <claudia.zirionmartinez@duke.edu>
2025-04-30 13:57:47 -0400

    run ploidy depth analysis

--- analyses/scripts/run_ploidy_depth.sh
+++ analyses/scripts/run_ploidy_depth.sh
@@ -5,11 +5,11 @@ mkdir -p analyses/notebooks/scripts/ploidy_depth/logs
 
 for set in "Desjardins" "Ashton"
 do
-    grep $set analyses/results/tables/duplications_putative.tsv |\
-    cut -f 5 |\
+    grep $set analyses/results/tables/ploidy_from_plots.tsv |\
+    cut -f 1 |\
     while read line
     do
-        quarto render analyses/scripts/ploidy_depth.qmd  -P sample_id:\"${line}\" -P dataset:\"${set}\" -P max_iterations:10 -P threshold_weight:0.2 -P threshold_norm_depth:5
+        quarto render analyses/scripts/ploidy_depth.qmd  -P sample_id:\"${line}\" -P dataset:\"${set}\" -P max_iterations:10000 -P threshold_weight:0.2 -P threshold_norm_depth:5
         mv analyses/notebooks/scripts/ploidy_depth.html analyses/notebooks/scripts/ploidy_depth/${line}.html
     done
 done
