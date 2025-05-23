db5d4acaafd744a95a0ae088091fa79107f78d79
Czirion <claudia.zirionmartinez@duke.edu>
2025-05-23 17:06:36 -0400

    fix path

--- scripts/get-chromosome-names.sh
+++ scripts/get-chromosome-names.sh
@@ -1,26 +1,26 @@
 #/usr/bin/env bash
 
-echo "lineage,accession,chromosome" > chromosome-names.csv
+echo "lineage,accession,chromosome" > Crypto_Desjardins/config/chromosomes.csv
 name=0
 for file in VNI.fasta VNBI.fasta
     do 
         lineage=$(echo $file | cut -d'.' -f1)
-        grep ">" data/references/$file | cut -d',' -f1 | cut -d'>' -f2 |  while read line
+        grep ">" Crypto_Desjardins/data/references/$file | cut -d',' -f1 | cut -d'>' -f2 |  while read line
         do 
             accession=$(echo $line | cut -d' ' -f1)
             name=$((name+1))
             echo $lineage,$accession,$name
         done
-    done >> chromosome-names.csv
+    done >> Crypto_Desjardins/config/chromosomes.csv
 
 for file in  VNII.fasta VNBII.fasta
     do 
         lineage=$(echo $file | cut -d'.' -f1)
-        grep ">" data/references/$file | cut -d',' -f1 | cut -d'>' -f2 |  while read line
+        grep ">" Crypto_Desjardins/data/references/$file | cut -d',' -f1 | cut -d'>' -f2 |  while read line
         do 
             accession=$(echo $line | cut -d' ' -f1)
             name=$(echo $line | rev | cut -d' ' -f1 | rev)
             echo $lineage,$accession,$name
         done
-    done >> chromosome-names.csv
+    done >> Crypto_Desjardins/config/chromosomes.csv
 
